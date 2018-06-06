9148bc6b7df19b99f20b78ed8601d32b4945b8f2
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-06-06 16:18:39 -0700

    Updated js file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(function() {
     .then(function(data) {
       data.data.forEach(function(name) {
         list.append(`<li>${name.title} (${name.url})</li>`);
-        console.log(name.title);
+        // console.log(name.title);
       });
     })
     .catch(function(error) {
@@ -21,3 +21,28 @@ $('#signUpLink').on('click', function() {
   $('#loginForm').hide();
   $('#signUpForm').slideToggle();
 });
+
+function logIn(event) {
+  event.preventDefault();
+  let data = {
+    data: {
+      username: $('#login_username').val(),
+      password: $('#login_password').val()
+    }
+  };
+
+  $.post('https://hack-or-snooze.herokuapp.com/auth', data, 'json')
+    .then(function(msg) {
+      localStorage.setItem('token', msg.data.token);
+      $('#loginForm').slideToggle();
+      $('#loginForm > form')[0].reset();
+      $('#loginLink').hide();
+      $('#signUpLink').hide();
+      console.log(msg);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+$('#loginForm').submit(logIn);
